0fea4f637e084d531a97e8be787d632dbfa8257c
Saleh Ahmed <46069811+thissaleh@users.noreply.github.com>
2021-05-28 06:30:05 +0800

    cmt

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_223xb9IAp', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3f8t5979lc810qg4a6ov1r53hq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://xppz6g05w1.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
